0b915fd874b045d3a1840645203d6576d3d48d3e
luizdevelopebr <l.eduardos.lopes@gmail.com>
2024-11-13 10:37:09 -0300

    migration update: add columns custo, tipo

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -10,7 +10,7 @@ pub fn run() {
     let migrations = vec![Migration {
         version: 1,
         description: "create table componente",
-        sql: "CREATE TABLE Componente (codigo INTEGER PRIMARY KEY, descricao TEXT, peso_liquido REAL);",
+        sql: "CREATE TABLE Componente (codigo INTEGER PRIMARY KEY, descricao TEXT, peso_liquido REAL, estoque REAL, custo REAL, tipo INTEGER);",
         kind: MigrationKind::Up
     }, Migration {
         version: 2,
@@ -25,18 +25,18 @@ pub fn run() {
     }, Migration {
         version: 4,
         description: "insert componentes",
-        sql:"INSERT INTO Componente (codigo, descricao, peso_liquido)
+        sql:"INSERT INTO Componente (codigo, descricao, peso_liquido, tipo, custo)
             VALUES 
-            (3605892, 'queijo ralado parmesão kg', 1.0),
-            (2736926, 'requeijão culinario 1,800kg', 1.8),
-            (87432, 'massa salgada kg (uso)', 1.0),
-            (22012, 'queijo mussarela p/ fatiar kg', 1.0);
+            (3605892, 'queijo ralado parmesão kg', 1.0, 1, 123.94),
+            (2736926, 'requeijão culinario 1,800kg', 1.8, 1, 49.51),
+            (87432, 'massa salgada kg (uso)', 1.0, 6, 3.32),
+            (22012, 'queijo mussarela p/ fatiar kg', 1.0, 1, 38.40);
             ",
         kind: MigrationKind::Up
     }, Migration {
         version: 5,
         description: "insert receita",
-        sql:"INSERT INTO Componente (codigo, descricao, peso_liquido) VALUES (12467, 'pão rech c/ queijo', NULL);",
+        sql:"INSERT INTO Componente (codigo, descricao, peso_liquido, tipo) VALUES (12467, 'pão rech c/ queijo', 1.0, 6);",
         kind: MigrationKind::Up
     }, Migration {
         version: 6,
@@ -47,11 +47,6 @@ pub fn run() {
         version: 7,
         description: "insert medidas dos coponentes",
         sql:"INSERT INTO Componente_Receita (receita_codigo, componente_codigo, medida) VALUES (12467, 3605892, 0.120), (12467, 2736926, 0.5), (12467, 87432, 12), (12467, 22012, 1.2);",
-        kind: MigrationKind::Up
-    }, Migration {
-        version: 8,
-        description: "add column estoque, custo_emb",
-        sql:"alter table Componente add column estoque REAL;alter table Componente add column custo_emb REAL;",
         kind: MigrationKind::Up
     }];
 
