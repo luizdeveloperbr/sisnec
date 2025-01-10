f446c37444163de49395d74175061e6ad5a72998
luizdeveloperbr <l.eduardos.lopes@gmail.com>
2025-01-10 12:41:47 -0300

    create MaterialUsoPage

---
+++ src/pages/MaterialUso/page.tsx
@@ -0,0 +1,37 @@
+import { IComponente } from "@/componentes/Componente/types";
+import Historico from "@/componentes/Historico";
+import Producao from "@/componentes/Producao";
+import Receita from "@/componentes/Receita"
+import { Tabs, TabsContent, TabsTrigger, TabsList } from "@/shadcn/ui/tabs";
+
+import { useLoaderData } from "react-router-dom";
+
+
+export default function PadariaPage() {
+	const produtos =
+		useLoaderData<
+			{ codigo: number; descricao: string; rendimento: number; componentes: IComponente[] }[]
+		>();
+	return (
+		<>
+			{produtos.map((produto) => (
+				<Tabs key={produto.codigo} defaultValue="producao">
+					<TabsList className="flex justify-center">
+						<TabsTrigger value="receita">Receita</TabsTrigger>
+						<TabsTrigger value="producao">Produção</TabsTrigger>
+						<TabsTrigger value="historico">Histórico Produzido</TabsTrigger>
+					</TabsList>
+					<TabsContent value="receita">
+						<Receita produto={produto} />
+					</TabsContent>
+					<TabsContent value="producao">
+						<Producao produto={produto} />
+					</TabsContent>
+					<TabsContent value="historico">
+						<Historico codigoInterno={produto.codigo} />
+					</TabsContent>
+				</Tabs>
+			))}
+		</>
+	);
+}
